e0cc79b68a771a58ec8afe0955ac95ac8d8b79f9
512143 <dan.kedrov@yandex.ru>
2021-09-23 18:31:49 +0300

    заметки 0.1
    1

---
+++ 512143/512143/Context/NoteContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _512143.Models;
+
+namespace _512143.Context
+{
+    public class NoteContext : DbContext
+    {
+        public NoteContext(DbContextOptions<NoteContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Note> Notes { get; set; }
+    }
+}
