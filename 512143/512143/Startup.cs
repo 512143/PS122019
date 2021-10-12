531827622eb051713a212d60ef43d1d48638e862
x0rzz <sobakatrade1@gmail.com>
2021-10-12 19:04:45 +0300

    corsi

--- 512143/512143/Startup.cs
+++ 512143/512143/Startup.cs
@@ -26,6 +26,7 @@ namespace _512143
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
             services.AddDbContext<NoteContext>(opt => opt.UseInMemoryDatabase("NameDataBase"));
             services.AddDbContext<UserContext>(opt => opt.UseInMemoryDatabase("NameDataBase"));
@@ -39,6 +40,8 @@ namespace _512143
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(option => option.WithOrigins("http://localhost:8080").AllowAnyMethod());
 
             app.UseRouting();
 
